996fa75e0f08d3b531bbcf6c9677f4dec1f07ec8
Shoan Chikato <sjchikato@gmail.com>
2022-09-25 21:12:22 +0200

    add more tests for stream serivces if stream exceed or go below limits

--- test/service/stream.service.test.js
+++ test/service/stream.service.test.js
@@ -25,4 +25,44 @@ describe("stream service", () => {
       streams: 3,
     });
   });
+
+  it("should return error after exceeding stream limit", () => {
+    const streamService = streamServiceFactory();
+
+    const user = {
+      id: 1,
+      name: "John",
+      surname: "Doe",
+      streams: 1,
+    };
+
+    const payload = {
+      action: "increase",
+      streams: 3,
+    };
+
+    expect(() => streamService.modifyUserStreams(payload, user)).toThrowError(
+      new Error("excessed streaming limit of 3")
+    );
+  });
+
+  it("should return error if streams are below zero", () => {
+    const streamService = streamServiceFactory();
+
+    const user = {
+      id: 1,
+      name: "John",
+      surname: "Doe",
+      streams: 1,
+    };
+
+    const payload = {
+      action: "decrease",
+      streams: 2,
+    };
+
+    expect(() => streamService.modifyUserStreams(payload, user)).toThrowError(
+      new Error("streams can't not be less than 0")
+    );
+  });
 });
