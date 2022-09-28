24c2e8833184bd27f19e27c51aafec14afbfad95
Shoan Chikato <sjchikato@gmail.com>
2022-09-28 14:48:17 +0200

    refactor placement of middleware and add cors and helmet middleware §

--- src/di/index.js
+++ src/di/index.js
@@ -1,3 +1,8 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerConfig = require("../swagger");
+const cors = require("cors");
+const helmet = require("helmet");
+
 const userRepoFactory = require("../repo/user.repo");
 const streamRouterFactory = require("../router/stream.router");
 const userRouterFactory = require("../router/user.router");
@@ -7,8 +12,10 @@ function appFactory() {
   const express = require("express");
   const app = express();
 
-  // body parser
   app.use(express.json());
+  app.use(cors());
+  app.use(helmet());
+  app.disable("x-powered-by");
 
   const userRepo = userRepoFactory();
 
@@ -19,6 +26,7 @@ function appFactory() {
 
   app.use("/users", userRouter);
   app.use("/streams", streamRouter);
+  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
   return app;
 }
--- src/server.js
+++ src/server.js
@@ -1,13 +1,8 @@
-const swaggerUi = require("swagger-ui-express");
-const swaggerConfig = require("./swagger");
-
 const appFactory = require("./di");
 const port = process.env.PORT || 3000;
 
 function server() {
   const app = appFactory();
-
-  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
   app.listen(port, () => console.log(`listening on port ${port}`));
 }
